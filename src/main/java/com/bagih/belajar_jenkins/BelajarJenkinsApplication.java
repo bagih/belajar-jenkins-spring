22f06e3e2860e84334fee6afaa0967cb4b86f30b
bagih <hartawanbagie@gmail.com>
2024-04-30 22:35:04 +0800

    initial commit

---
+++ src/main/java/com/bagih/belajar_jenkins/BelajarJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.bagih.belajar_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagih/belajar_jenkins/BelajarJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagih.belajar_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
